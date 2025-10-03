c860c835ae646239a232bf7d74fa48c7ab3e4899
altarterminal <118420278+nm-jp@users.noreply.github.com>
2025-10-03 14:18:54 +0900

    add interface

--- sh/sequence_control/database_control_wrapper.sh
+++ sh/sequence_control/database_control_wrapper.sh
@@ -8,7 +8,19 @@ me_insert_evaldata_to_database() {
 
   me_this_dir=$(dirname "${BASH_SOURCE[0]}")
   me_script_dir=$(dirname "${me_this_dir}")
-  me_database_dir=${me_script_dir}/database_control
+  me_database_dir="${me_script_dir}/database_control"
 
   "${me_database_dir}/insert_evaldata_to_database.sh" "$@"
+}
+
+me_supplement_evaldata() {
+  local me_this_dir
+  local me_script_dir
+  local me_database_dir
+
+  me_this_dir=$(dirname "${BASH_SOURCE[0]}")
+  me_script_dir=$(dirname "${me_this_dir}")
+  me_database_dir="${me_script_dir}/supplement_evaldata"
+  
+  "${me_database_dir}/supplement_evaldata.sh" "$@"
 }
